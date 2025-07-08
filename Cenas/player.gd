0d4def7171f464d103fd9cc7bd21b681b88c0bfa
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2025-07-07 22:37:50 -0300

    SO BORA

--- Cenas/player.gd
+++ Cenas/player.gd
@@ -1,22 +1,12 @@
 extends CharacterBody2D
 
-
+const ACCELERATION = 1000
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
-
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var x :=  Input.get_axis("ui_left", "ui_right")
+	var y :=  Input.get_axis("ui_up", "ui_down")
+	velocity.x = move_toward(velocity.x, x*SPEED, ACCELERATION*delta)
+	velocity.y = move_toward(velocity.y, y*SPEED, ACCELERATION*delta)
 	move_and_slide()
